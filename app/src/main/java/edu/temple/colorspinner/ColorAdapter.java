8b359228c330139ad6da08fbf7c17bcd561ff8f4
Kyle Hastings <kylejhastings@gmail.com>
2019-09-30 19:32:33 -0400

    The ColorAdapter class for colorspinner

---
+++ app/src/main/java/edu/temple/colorspinner/ColorAdapter.java
@@ -0,0 +1,36 @@
+package edu.temple.colorspinner;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+class ColorAdapter extends BaseAdapter {
+    private Context context;
+    private String[] colors;
+
+    public ColorAdapter(Context context, String[] colors) {
+        this.context = context;
+        this.colors = colors;
+    }
+
+    @Override
+    public int getCount() {
+        return colors.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return colors[i];
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        return null;
+    }
+}
